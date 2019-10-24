b56d30e4e103b1858c0693a6f44bf6675cc0b559
Koichiro IWAO <meta@vmeta.jp>
2019-10-24 10:58:54 +0900

    GuestReg: add validation

--- app/models/guest_reg.rb
+++ app/models/guest_reg.rb
@@ -1,2 +1,9 @@
 class GuestReg < ApplicationRecord
+
+  validates :full_name, presence: true
+  validates :organization, presence: true
+  validates :email, presence: true
+  validates :purpose, presence: true
+  validates :escortant, presence: true
+  
 end
