2551805b5af28cb06e4798b7705dc0389a46cbf1
Koichiro IWAO <meta@vmeta.jp>
2019-10-31 15:29:43 +0900

    Timezone is JST

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Iwao
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
