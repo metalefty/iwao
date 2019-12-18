25517307b29b9bfbb54fe8fc2bca20b3b327211b
Koichiro IWAO <meta@vmeta.jp>
2019-12-18 18:11:25 +0900

    aws: switch to rails credentials

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,2 +1,5 @@
-creds = Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+creds = Aws::Credentials.new(
+  Rails.application.credentials.dig(:aws, :access_key_id),
+  Rails.application.credentials.dig(:aws, :secret_access_key)
+)
 Aws::Rails.add_action_mailer_delivery_method(:ses, credentials: creds, region: 'us-west-2')
