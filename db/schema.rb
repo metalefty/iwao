581e37d86b4536a54d76162944bc2a1ec3fcf725
Koichiro IWAO <meta@vmeta.jp>
2019-12-18 15:56:02 +0900

    forgot to commit schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_014458) do
+ActiveRecord::Schema.define(version: 2019_12_18_063620) do
 
   create_table "guest_regs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "full_name"
