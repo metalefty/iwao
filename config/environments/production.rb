9dab9e0b3f91e8161d73627d9f93e1a6610314c2
Koichiro IWAO <meta@vmeta.jp>
2019-12-18 14:21:54 +0900

    production: add default actionmailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,12 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # UNCOMMENT THIS
+  #config.action_mailer.perform_caching = true
+  #config.action_mailer.default_url_options = {
+  #  host: 'iwao.example.com',
+  #  protocol: :https
+  #}
+  #config.action_mailer.delivery_method = :ses
 end
