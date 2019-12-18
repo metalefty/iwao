116732f42275fc534baade5334b62861cd13decd
Koichiro IWAO <meta@vmeta.jp>
2019-12-18 14:41:59 +0900

    add config.hosts

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Iwao
     config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    # SET THIS
+    config.hosts << "iwao.example.com"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
